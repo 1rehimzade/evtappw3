3f0065043a62dd7ad042e612280cb29299512c12
nihatab104 <146129654+nihatab104@users.noreply.github.com>
2023-10-06 05:38:27 +0400

    first commit

---
+++ w3lasttask1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] myArray = { 11, 23, 39, 74, 51 };
+        int[] valuesToAdd = { 63, 72, 88 };
+
+        InsertArray(ref myArray, valuesToAdd);
+
+        Console.WriteLine("yenilenmis,kohne arr:");
+        PrintArray(myArray);
+    }
+
+    static void InsertArray(ref int[] array, int[] values)
+    {
+        int oldLength = array.Length;
+        int newLength = oldLength + values.Length;
+
+        int[] newArray = new int[newLength];
+
+        for (int i = 0; i < oldLength; i++)
+        {
+            newArray[i] = array[i];
+        }
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            newArray[oldLength + i] = values[i];
+        }
+
+        array = newArray; 
+    }
+
+    static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
